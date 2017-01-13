2456337941f1fccc327ae6bdaa4f9434e6889fb3
jukkapongtor <jigsaw1011@gmail.com>
2017-01-13 20:14:05 +0700

    create new moumfern

---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+    session_start();
+    date_default_timezone_set('Asia/Bangkok');
+    $module=empty($_GET['module'])?"":$_GET['module'];
+    $action=empty($_GET['action'])?"":$_GET['action'];
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>MOUMFERN SHOP</title>
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+ <meta name="keywords" content="ขายเฟิร์น,ขายต้นไม้,เฟิร์น,ต้นไม้,ขายเฟิร์น จังหวัดเชียงใหม่,ขายเฟิร์น รับส่งทั่วประเทศ,ร้านขายเฟิร์น,มุมเฟิร์น ขายส่งเฟิร์น,ขายเฟิร์น ตลาดคำเที่ยง,ขายต้นไม้ ตลาดคำเที่ยง">
+ <link rel="shortcut icon" href="images/icon/logomumfern.png" />
+ <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+ <link rel="stylesheet" type="text/css" href="css/mystyle.css">
+ <link rel="stylesheet" type="text/css" href="css/jquery-ui.css">
+ <link rel="stylesheet" type="text/css" href="css/sweetalert.css">
+ <script src="js/jquery-1.11.3.min.js"></script>
+ <script src="js/jquery-ui.js"></script>
+ <script src="js/bootstrap.min.js"></script>
+ <script src="js/sweetalert.min.js"></script> 
+</head>
+<body>
+<div class="header">
+    <div class="container-fluid">
+        <div class="col-md-3 logo">MOUMFERN</div>
+        <div class="col-md-6"></div>
+        <div class="col-md-3">
+            <div class="header_menu_home">
+                <h4>หน้าหลัก</h4>
+            </div>
+            <div class="header_menu_product">
+                <h4>สินค้า</h4>
+            </div>
+            <div class="header_menu_news">
+                <h4>ข่าวสาร</h4>
+            </div>
+            <div class="header_menu_webboard">
+                <h4>เว็บบอร์ด</h4>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container-fluuid">
+    <div class="col-md-2"></div>
+    <div class="col-md-8" style="background:#fff;height:100px;"> asdadasdasd</div>
+    <div class="col-md-2"></div>
+</div>
+</body>
+</html>
